1301d90732cfb481c7253848c9ad9427ef5ad016
ocoanet <olivier.coanet@gmail.com>
2016-10-18 20:45:11 +0200

    Add simple client-server test

---
+++ Abc.Zerio.Tests/ClientServer/ClientServerFixture.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using Abc.Zerio.Core;
+using Abc.Zerio.Serialization;
+using Abc.Zerio.Tests.Utils;
+using NUnit.Framework;
+
+namespace Abc.Zerio.Tests.ClientServer
+{
+    [TestFixture]
+    public abstract class ClientServerFixture
+    {
+        [SetUp]
+        public virtual void Setup()
+        {
+            ClientConfiguration = new ClientConfiguration();
+            ServerConfiguration = new ServerConfiguration(TcpUtil.GetRandomUnusedPort());
+            ServerEndPoint = new IPEndPoint(IPAddress.Loopback, ServerConfiguration.ListeningPort);
+
+            var serializationRegistries = new SerializationRegistries();
+            ConfigureSerialization(serializationRegistries);
+
+            Client = new RioClient(ClientConfiguration, new SerializationEngine(serializationRegistries.Client));
+            Server = new RioServer(ServerConfiguration, new SessionManager(ServerConfiguration), new SerializationEngine(serializationRegistries.Server));
+        }
+
+        protected virtual void ConfigureSerialization(SerializationRegistries registries)
+        {
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            Client.Dispose();
+            Server.Dispose();
+        }
+
+        protected ServerConfiguration ServerConfiguration { get; set; }
+        protected ClientConfiguration ClientConfiguration { get; set; }
+        protected RioClient Client { get; set; }
+        protected RioServer Server { get; set; }
+        protected IPEndPoint ServerEndPoint { get; private set; }
+
+        protected class SerializationRegistries
+        {
+            public SerializationRegistry Client { get; } = new SerializationRegistry(Encoding.ASCII);
+            public SerializationRegistry Server { get; } = new SerializationRegistry(Encoding.ASCII);
+
+            public void ForBoth(Action<SerializationRegistry> action)
+            {
+                action.Invoke(Client);
+                action.Invoke(Server);
+            }
+        }
+    }
+}
---
+++ Abc.Zerio.Tests/ClientServer/PingPongTests.cs
@@ -0,0 +1,70 @@
+﻿using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using Abc.Zerio.Serialization;
+using NUnit.Framework;
+
+namespace Abc.Zerio.Tests.ClientServer
+{
+    public class PingPongTests : ClientServerFixture
+    {
+        [Test]
+        public void should_receive_pong_message()
+        {
+            Server.MessageReceived += (clientId, m) => Server.Send(clientId, new Pong { PingId = ((Ping)m).Id });
+            Server.Start();
+
+            var taskCompletionSource = new TaskCompletionSource<Pong>();
+            Client.MessageReceived += m => taskCompletionSource.SetResult((Pong)m);
+
+            Client.Connect(ServerEndPoint);
+            Client.Send(new Ping { Id = 9876 });
+
+            var received = taskCompletionSource.Task.Wait(200);
+            Assert.That(received, Is.True);
+            Assert.That(taskCompletionSource.Task.Result.PingId, Is.EqualTo(9876));
+        }
+
+        protected override void ConfigureSerialization(SerializationRegistries registries)
+        {
+            registries.ForBoth(r => r.AddMapping<Ping, PingSerializer>());
+            registries.ForBoth(r => r.AddMapping<Pong, PongSerializer>());
+        }
+
+        public class Ping
+        {
+            public long Id { get; set; }
+        }
+
+        public class Pong
+        {
+            public long PingId { get; set; }
+        }
+
+        public class PingSerializer : IBinaryMessageSerializer
+        {
+            public void Serialize(object message, BinaryWriter binaryWriter)
+            {
+                binaryWriter.Write(((Ping)message).Id);
+            }
+
+            public void Deserialize(object message, BinaryReader binaryReader)
+            {
+                ((Ping) message).Id = binaryReader.ReadInt32();
+            }
+        }
+
+        public class PongSerializer : IBinaryMessageSerializer
+        {
+            public void Serialize(object message, BinaryWriter binaryWriter)
+            {
+                binaryWriter.Write(((Pong)message).PingId);
+            }
+
+            public void Deserialize(object message, BinaryReader binaryReader)
+            {
+                ((Pong)message).PingId = binaryReader.ReadInt32();
+            }
+        }
+    }
+}
