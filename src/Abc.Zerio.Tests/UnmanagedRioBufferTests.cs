e6603737abaacb4a99aaaf5e72b05ff9267419ec
Romain Verdier <r.verdier@abc-arbitrage.com>
2019-12-23 16:39:51 +0100

    Add basic tests for UnmanagedRioBuffer and fix pointer arithmetic

---
+++ src/Abc.Zerio.Tests/UnmanagedRioBufferTests.cs
@@ -0,0 +1,52 @@
+using System;
+using Abc.Zerio.Core;
+using Abc.Zerio.Interop;
+using NUnit.Framework;
+
+namespace Abc.Zerio.Tests
+{
+    [TestFixture]
+    public unsafe class UnmanagedRioBufferTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            WinSock.EnsureIsInitialized();
+        }
+
+        [Test]
+        public void should_allocate_unmanaged_buffer()
+        {
+            // Act
+            using var buffer = new UnmanagedRioBuffer<RequestEntry>(5, 1024);
+            
+            // Assert
+            Assert.AreEqual(5, buffer.Length);
+            Assert.AreEqual(sizeof(RequestEntry) + 1024, buffer.EntryReservedSpaceSize);
+            Assert.AreNotEqual(IntPtr.Zero, (IntPtr)buffer.FirstEntry);
+        }
+
+        [Test]
+        public void should_allow_access_and_mutation_of_entries()
+        {
+            // Arrange
+            using var buffer = new UnmanagedRioBuffer<RequestEntry>(5, 1024);
+            
+            // Act
+            for (var i = 0; i < buffer.Length; i++)
+            {
+                buffer[i]->Type = RequestType.Receive;
+                buffer[i]->SessionId = 42;
+                buffer[i]->BufferSegmentId = 123;
+            }
+            
+            // Assert
+            for (var i = 0; i < buffer.Length; i++)
+            {
+                Assert.AreEqual(RequestType.Receive, buffer[i]->Type);
+                Assert.AreEqual(42, buffer[i]->SessionId);
+                Assert.AreEqual(123, buffer[i]->BufferSegmentId);
+            }
+        }
+    }
+}
--- src/Abc.Zerio/Core/UnmanagedRioBuffer.cs
+++ src/Abc.Zerio/Core/UnmanagedRioBuffer.cs
@@ -15,32 +15,35 @@ namespace Abc.Zerio.Core
         public int EntryReservedSpaceSize { get; }
         public int Length { get; }
 
-        public T* this[in int index] => FirstEntry + index * EntryReservedSpaceSize;
+        public T* this[in int index] => (T*)((byte*)FirstEntry + index * EntryReservedSpaceSize);
 
         public UnmanagedRioBuffer(int bufferCount, int bufferLength)
         {
             // Example with:
             //  - Length = 2 entries
             //  - BufferLength = 1024 bytes
-            //  - Entry of type T having a single int field
+            //  - Entry of type T having
+            //     - A 1 byte field
+            //     - A 4 bytes field
+            //     - A 4 bytes field
             //
-            //|-- Padding --|- Entry reserved space --------------------------------------------------------|- Entry reserved space  -------------------------------------------------------|-- Padding --|   
-            //|  128 bytes  |                                                                               |                                                                               |  128 bytes  |  
-            //|             |- Entry #1 -----------------------------------------------|                    |- Entry #2 -----------------------------------------------|                    |             |  
-            //|             |                                                          |                    |                                                          |                    |             |  
-            //|             |           |- RIO_BUF ------------------------------------|- Data - ... -------|           |- RIO_BUF ------------------------------------|- Data - ... -------|             |  
-            //|             |           |                                              |                    |           |                                              |                    |             |  
-            //|            [0]         [4]                    [12]        [16]        [20]                 [1044]      [1048]                 [1056]      [1060]      [1064]                |             |  
-            //|             +-----------+----------------------+-----------+-----------+-------- ... -------+-----------+----------------------+-----------+-----------+-------- ... -------+             |  
-            //|             | IntField  | BufferId             | Offset    | Length    | Buffer segment     | IntField  | BufferId             | Offset    | Length    | Buffer segment     |             |  
-            //|             |           | IntPtr               | int       | int       | bytes              |           | IntPtr               | int       | int       | bytes              |             |  
-            //|             | 4 bytes   | 8 bytes              | 4         | 4         | 1024               | 4 bytes   | 8 bytes              | 4         | 4         | 1024               |             |  
-            //|             +-----------+----------------------+-----------+-----------+-------- ... -------+-----------+----------------------+-----------+-----------+-------- ... -------+             | 
-            //|             |                                  | 20        | 1024      |                                                       | 1064      | 1024      ||               
-            //|                                                  +---------------------^                                                         +---------------------^
+            // |-- Padding --|- Entry reserved space --------------------------------------------------------|- Entry reserved space  -------------------------------------------------------|-- Padding --|   
+            // |  128 bytes  |                                                                               |                                                                               |  128 bytes  |  
+            // |             |- Entry #1 -----------------------------------------------|                    |- Entry #2 -----------------------------------------------|                    |             |  
+            // |             |                                                          |                    |                                                          |                    |             |  
+            // |             |           |- RIO_BUF ------------------------------------|- Data - ... -------|           |- RIO_BUF ------------------------------------|- Data - ... -------|             |  
+            // |             |           |                                              |                    |           |                                              |                    |             |  
+            //[0]           [128]       [137]                 [145]       [149]       [153]                [1177]      [1186]                 [1194]      [1198]      [1202]                 |             |  
+            // |             +-----------+----------------------+-----------+-----------+-------- ... -------+-----------+----------------------+-----------+-----------+-------- ... -------+             |  
+            // |             | Fields    | BufferId             | Offset    | Length    | Buffer segment     | Fields    | BufferId             | Offset    | Length    | Buffer segment     |             |  
+            // |             |           | IntPtr               | int       | int       | bytes              |           | IntPtr               | int       | int       | bytes              |             |  
+            // |             | 9 bytes   | 8 bytes              | 4         | 4         | 1024               | 9 bytes   | 8 bytes              | 4         | 4         | 1024               |             |  
+            // |             +-----------+----------------------+-----------+-----------+-------- ... -------+-----------+----------------------+-----------+-----------+-------- ... -------+             | 
+            // |             |                                  | 153       | 1024      |                                                       | 1202      | 1024      |               
+            // |                                                  +---------------------^                                                         +---------------------^
             
             Length = bufferCount;
-            var entryLength = sizeof(RequestEntry);
+            var entryLength = sizeof(T);
             EntryReservedSpaceSize = entryLength + bufferLength;
 
             // Padding is added at the start and the end of the buffer to prevent false sharing when using this UnmanagedRioBuffer
@@ -54,16 +57,14 @@ namespace Abc.Zerio.Core
             if (_bufferId == WinSock.Consts.RIO_INVALID_BUFFERID)
                 WinSock.ThrowLastWsaError();
 
-            FirstEntry = (T*)_buffer.ToPointer() + _padding;
-
-            var currentEntry = FirstEntry;
+            var currentEntry = FirstEntry = (T*)((byte*)_buffer.ToPointer() + _padding);
 
             for (var i = 0; i < bufferCount; i++)
             {
                 var bufferDescriptor = currentEntry->GetRioBufferDescriptor();
                 
                 bufferDescriptor->BufferId = _bufferId;
-                bufferDescriptor->Offset = i * EntryReservedSpaceSize + sizeof(T);
+                bufferDescriptor->Offset = _padding + i * EntryReservedSpaceSize + sizeof(T);
                 bufferDescriptor->Length = bufferLength;
                 
                 currentEntry = (T*)((byte*)(currentEntry + 1) + bufferLength);
