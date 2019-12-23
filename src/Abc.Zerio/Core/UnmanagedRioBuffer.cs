545651006c0e11938f3ef2a91970f282d91552f8
Romain Verdier <r.verdier@abc-arbitrage.com>
2019-12-23 09:49:21 +0100

    Add padding at the start and the end of the UnmanagedRioBuffer

--- src/Abc.Zerio/Core/UnmanagedRioBuffer.cs
+++ src/Abc.Zerio/Core/UnmanagedRioBuffer.cs
@@ -6,10 +6,12 @@ namespace Abc.Zerio.Core
     internal sealed unsafe class UnmanagedRioBuffer<T> : IDisposable
         where T : unmanaged, IRioBufferSegmentDescriptorContainer
     {
+        private const int _padding = 128;
+        
         private readonly IntPtr _buffer;
         private readonly IntPtr _bufferId;
 
-        public T* FirstEntry => (T*)_buffer.ToPointer();
+        public T* FirstEntry { get; } 
         public int EntryReservedSpaceSize { get; }
         public int Length { get; }
 
@@ -18,29 +20,32 @@ namespace Abc.Zerio.Core
         public UnmanagedRioBuffer(int bufferCount, int bufferLength)
         {
             // Example with:
-            //  - bufferLength = 1024
+            //  - Length = 2 entries
+            //  - BufferLength = 1024 bytes
             //  - Entry of type T having a single int field
             //
-            //  |- Entry reserved space --------------------------------------------------------|- Entry reserved space  -------------------------------------------------------|
-            //  |                                                                               |                                                                               |
-            //  |- Entry #1 -----------------------------------------------|                    |- Entry #2 -----------------------------------------------|                    |
-            //  |                                                          |                    |                                                          |                    |
-            //  |           |- RIO_BUF ------------------------------------|- Data - ... -------|           |- RIO_BUF ------------------------------------|- Data - ... -------|
-            //  |           |                                              |                    |           |                                              |                    |
-            // [0]         [4]                    [12]        [16]        [20]                 [1044]      [1048]                 [1056]      [1060]      [1064]                |
-            //  +-----------+----------------------+-----------+-----------+-------- ... -------+-----------+----------------------+-----------+-----------+-------- ... -------+
-            //  | IntField  | BufferId             | Offset    | Length    | Buffer segment     | IntField  | BufferId             | Offset    | Length    | Buffer segment     |
-            //  |           | IntPtr               | int       | int       | bytes              |           | IntPtr               | int       | int       | bytes              |
-            //  | 4 bytes   | 8 bytes              | 4         | 4         | 1024               | 4 bytes   | 8 bytes              | 4         | 4         | 1024               |
-            //  +-----------+----------------------+-----------+-----------+-------- ... -------+-----------+----------------------+-----------+-----------+-------- ... -------+
-            //  |                                  | 20        | 1024      |                                                       | 1064      | 1024      |
-            //                                       +---------------------^                                                         +---------------------^
+            //|-- Padding --|- Entry reserved space --------------------------------------------------------|- Entry reserved space  -------------------------------------------------------|-- Padding --|   
+            //|  128 bytes  |                                                                               |                                                                               |  128 bytes  |  
+            //|             |- Entry #1 -----------------------------------------------|                    |- Entry #2 -----------------------------------------------|                    |             |  
+            //|             |                                                          |                    |                                                          |                    |             |  
+            //|             |           |- RIO_BUF ------------------------------------|- Data - ... -------|           |- RIO_BUF ------------------------------------|- Data - ... -------|             |  
+            //|             |           |                                              |                    |           |                                              |                    |             |  
+            //|            [0]         [4]                    [12]        [16]        [20]                 [1044]      [1048]                 [1056]      [1060]      [1064]                |             |  
+            //|             +-----------+----------------------+-----------+-----------+-------- ... -------+-----------+----------------------+-----------+-----------+-------- ... -------+             |  
+            //|             | IntField  | BufferId             | Offset    | Length    | Buffer segment     | IntField  | BufferId             | Offset    | Length    | Buffer segment     |             |  
+            //|             |           | IntPtr               | int       | int       | bytes              |           | IntPtr               | int       | int       | bytes              |             |  
+            //|             | 4 bytes   | 8 bytes              | 4         | 4         | 1024               | 4 bytes   | 8 bytes              | 4         | 4         | 1024               |             |  
+            //|             +-----------+----------------------+-----------+-----------+-------- ... -------+-----------+----------------------+-----------+-----------+-------- ... -------+             | 
+            //|             |                                  | 20        | 1024      |                                                       | 1064      | 1024      ||               
+            //|                                                  +---------------------^                                                         +---------------------^
             
             Length = bufferCount;
             var entryLength = sizeof(RequestEntry);
             EntryReservedSpaceSize = entryLength + bufferLength;
 
-            var totalBufferLength = (uint)(bufferCount * EntryReservedSpaceSize);
+            // Padding is added at the start and the end of the buffer to prevent false sharing when using this UnmanagedRioBuffer
+            // as the underlying ring buffer memory of an unmanaged disruptor
+            var totalBufferLength = (uint)(_padding + bufferCount * EntryReservedSpaceSize + _padding);
 
             const int allocationType = Kernel32.Consts.MEM_COMMIT | Kernel32.Consts.MEM_RESERVE;
             _buffer = Kernel32.VirtualAlloc(IntPtr.Zero, totalBufferLength, allocationType, Kernel32.Consts.PAGE_READWRITE);
@@ -48,6 +53,8 @@ namespace Abc.Zerio.Core
             _bufferId = WinSock.Extensions.RegisterBuffer(_buffer, totalBufferLength);
             if (_bufferId == WinSock.Consts.RIO_INVALID_BUFFERID)
                 WinSock.ThrowLastWsaError();
+
+            FirstEntry = (T*)_buffer.ToPointer() + _padding;
 
             var currentEntry = FirstEntry;
 
