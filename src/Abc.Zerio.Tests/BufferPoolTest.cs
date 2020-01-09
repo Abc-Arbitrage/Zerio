1e324fe903b03615e9478bef881b2be8dfa89802
Victor Baybekov <v.baybekov@abc-arbitrage.com>
2020-01-09 17:42:09 +0100

    Add BoundedLocalPool (single producer multi consumer bounded queue)

--- src/Abc.Zerio.Tests/BufferPoolTest.cs
+++ src/Abc.Zerio.Tests/BufferPoolTest.cs
@@ -5,6 +5,7 @@ using System.Diagnostics;
 using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Tasks;
+using Abc.Zerio.Alt.Buffers;
 using Abc.Zerio.Core;
 using Abc.Zerio.Interop;
 using NUnit.Framework;
@@ -37,6 +38,8 @@ namespace Abc.Zerio.Tests
             {
                 cb2.TryTake(out _);
             }
+
+            GC.Collect(2, GCCollectionMode.Forced, true);
 
             List<Task> tasks = new List<Task>();
             var ticks = Stopwatch.GetTimestamp();
@@ -96,6 +99,8 @@ namespace Abc.Zerio.Tests
                 cb2.TryDequeue(out _);
             }
 
+            GC.Collect(2, GCCollectionMode.Forced, true);
+
             List<Task> tasks = new List<Task>();
             var ticks = Stopwatch.GetTimestamp();
             Console.WriteLine($"GC: {GC.CollectionCount(0)} - {GC.CollectionCount(1)} - {GC.CollectionCount(2)}");
@@ -152,6 +157,68 @@ namespace Abc.Zerio.Tests
             {
                 cb2.TryPop(out _);
             }
+
+            GC.Collect(2, GCCollectionMode.Forced, true);
+
+            List<Task> tasks = new List<Task>();
+            var ticks = Stopwatch.GetTimestamp();
+            Console.WriteLine($"GC: {GC.CollectionCount(0)} - {GC.CollectionCount(1)} - {GC.CollectionCount(2)}");
+
+            for (int i = 0; i < 16; i++)
+            {
+                tasks.Add(Task.Factory.StartNew(() =>
+                {
+                    while (true)
+                    {
+                        if (Interlocked.Increment(ref count) > iterations)
+                        {
+                            return;
+                        }
+
+                        if (cb2.TryPop(out var ts))
+                        {
+                            cb1.Push(ts);
+                        }
+
+                        if (cb1.TryPop(out ts))
+                        {
+                            cb2.Push(ts);
+                        }
+                    }
+                },
+                                                TaskCreationOptions.LongRunning));
+            }
+
+            Task.WhenAll(tasks).Wait();
+            ticks = Stopwatch.GetTimestamp() - ticks;
+            Console.WriteLine($"GC: {GC.CollectionCount(0)} - {GC.CollectionCount(1)} - {GC.CollectionCount(2)}");
+            var seconds = ticks * 1.0 / Stopwatch.Frequency;
+            var mops = iterations / seconds / 1_000_000;
+            Console.WriteLine($"Count: {count}");
+            Console.WriteLine($"MOPS: {2 * mops:N2}");
+        }
+
+
+        [Test, Explicit("Long running")]
+        public void BoundedLocalPoolTest()
+        {
+            int iterations = 100_000_000;
+            int count = 0;
+            var capacity = 64 * 1024;
+            var cb1 = new BoundedLocalPool<TestStruct>(capacity);
+            var cb2 = new BoundedLocalPool<TestStruct>(capacity);
+            for (int i = 0; i < capacity; i++)
+            {
+                cb1.Push(new TestStruct());
+                cb2.Push(new TestStruct());
+            }
+
+            for (int i = 0; i < capacity; i++)
+            {
+                cb2.TryPop(out _);
+            }
+
+            GC.Collect(2, GCCollectionMode.Forced, true);
 
             List<Task> tasks = new List<Task>();
             var ticks = Stopwatch.GetTimestamp();
---
+++ src/Abc.Zerio/Alt/Buffers/BoundedLocalPool.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Threading;
+
+namespace Abc.Zerio.Alt.Buffers
+{
+    public class BoundedLocalPool<T>
+    {
+        private readonly int _capacity;
+        private readonly T[] _items;
+        private int _mask;
+
+        // long is enough for 68 years at int.Max operations per second.
+        // [... head<- items -> tail ...]
+        private long _tail;
+        private long _head;
+
+        private SpinLock _spinLock = new SpinLock(false);
+
+        public BoundedLocalPool(int capacity)
+        {
+            if (!Utils.IsPowerOfTwo(capacity))
+                throw new ArgumentException("Capacity must be a power of 2");
+            _capacity = capacity;
+            _items = new T[_capacity];
+            _mask = _capacity - 1;
+        }
+
+        public int Count
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => (int)(Volatile.Read(ref _tail) - Volatile.Read(ref _head));
+        }
+
+        /// <summary>
+        /// Called by a single <see cref="Poller"/> thread. 
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Push(T item)
+        {
+            if (Count == _capacity)
+                ThrowExceededCapacity();
+            var idx = (int)(_tail & _mask);
+            _items[idx] = item;
+            Volatile.Write(ref _tail, Volatile.Read(ref _tail) + 1);
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static void ThrowExceededCapacity()
+        {
+            throw new InvalidOperationException("Exceeded capacity");
+        }
+
+        /// <summary>
+        /// Only multiple sends per <see cref="Session"/> could compete. Single send should never spin.
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        public bool TryPop(out T item)
+        {
+            item = default;
+            if (Count == 0)
+                return false;
+
+            var taken = false;
+            try
+            {
+                _spinLock.Enter(ref taken);
+
+                if (Count == 0)
+                    return false;
+
+                var idx = (int)(_head & _mask);
+                item = _items[idx];
+                Volatile.Write(ref _head, Volatile.Read(ref _head) + 1);
+            }
+            finally
+            {
+                if (taken)
+                    _spinLock.Exit(useMemoryBarrier: false);
+            }
+
+            return false;
+        }
+    }
+}
